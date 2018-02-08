c26232379223019074f9d59fcc69c91bfe3e4d0b
richabhatia20 <bhatia.ri@husky.neu.edu>
2018-02-08 15:34:37 -0500

    added json schema validation

---
+++ richademo1/src/main/java/com/neu/info7255/demo/Richademo1Application.java
@@ -0,0 +1,34 @@
+package com.neu.info7255.demo;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+
+import com.github.fge.jsonschema.core.exceptions.ProcessingException;
+import com.neu.info7255.demo.validation.ValidationUtils;
+
+@SpringBootApplication
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+public class Richademo1Application {
+
+	public static void main(String[] args) throws IOException, ProcessingException {
+		
+		File schemaFile = new File("/Users/richabhatia/Downloads/richademo1/src/main/resources/static/schema.json");
+		File jsonFile = new File("/Users/richabhatia/Downloads/richademo1/src/main/resources/static/data.json");
+		 if (ValidationUtils.isJsonValid(schemaFile, jsonFile)){
+		    	System.out.println("Valid!");
+		    }else{
+		    	System.out.println("NOT valid!");
+		    }
+		
+		
+		
+		
+		SpringApplication.run(Richademo1Application.class, args);
+	}
+}
---
+++ richademo1/src/main/java/com/neu/info7255/demo/config/RedisConfig.java
@@ -0,0 +1,38 @@
+package com.neu.info7255.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import com.neu.info7255.demo.Person;
+import com.neu.info7255.demo.repository.PersonRepository;
+
+@Configuration
+public class RedisConfig {
+	
+	@Bean
+	public PersonRepository repository(RedisTemplate<String, Person> redisTemplate){
+		return new PersonRepository(redisTemplate);
+		
+	}
+	@Bean
+	public RedisTemplate<String, Person> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+		RedisTemplate<String, Person> template = new RedisTemplate();
+
+		template.setConnectionFactory(redisConnectionFactory);
+
+		RedisSerializer<String> stringSerializer = new StringRedisSerializer();
+		RedisSerializer<Person> personSerializer = new Jackson2JsonRedisSerializer<>(Person.class);
+
+		template.setKeySerializer(stringSerializer);
+		template.setValueSerializer(personSerializer);
+		template.setHashKeySerializer(stringSerializer);
+		template.setHashValueSerializer(personSerializer);
+
+		return template;
+	}
+}
